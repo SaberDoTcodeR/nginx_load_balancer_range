d17b404525d3d99039508a5972414c9d632ae7c4
ahmadsalimi <ahsa9978@gmail.com>
2020-04-05 17:40:59 +0430

    change say to log

--- src/script.lua
+++ src/script.lua
@@ -1,17 +1,36 @@
+local match = ngx.re.match
+
+
+local start = 0
+local stop = -1
+content_length = 1000000
+
 -- parse range header
 local range_header = ngx.req.get_headers()["Range"] or "bytes=0-"
 local matches, err = match(range_header, "^bytes=(\\d+)?-([^\\\\]\\d+)?", "joi")
 if matches then
 	if matches[1] == nil and matches[2] then
-		stop = (origin_headers["Content-Length"] - 1)
+		stop = (content_length - 1)
 		start = (stop - matches[2]) + 1
 	else
 		start = matches[1] or 0
-		stop = matches[2] or (origin_headers["Content-Length"] - 1)
+		stop = matches[2] or (content_length - 1)
 	end
 else
-	stop = (origin_headers["Content-Length"] - 1)
+	stop = (content_length - 1)
 end
 
-ngx.say(start)
-ngx.say(stop)
+ngx.log(ngx.NOTICE, start)
+ngx.log(ngx.NOTICE, stop)
+
+-- function get_server_index(start, stop)
+--     if stop < content_length / 3 then
+--         return 0
+--     else if start >= content_length / 3 and stop < content_length * 2 / 3 then
+--         return 1
+--     else if start >= content_length * 2 / 3 then
+--         return 2
+--     end
+-- end
+
+-- print(get_server_index(0,345660 ))
